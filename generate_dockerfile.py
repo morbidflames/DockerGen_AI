8287be3cb29bc5b77be615b8d7004352cc545907
morbidflames <tarunsg1106@gmail.com>
2025-08-21 21:50:10 +0530

    Final Commit

---
+++ generate_dockerfile.py
@@ -0,0 +1,23 @@
+import ollama
+
+PROMPT = """
+ONLY Generate an ideal dockerfile for {language} with best practices. Do not provide any description
+Include:
+- Base image
+- Installing dependencies
+- Setting working directory
+- Adding source code
+- Running the application
+- Multi stage docker build
+ """
+
+def generate_dockerfile(language):
+    response = ollama.chat(model='llama3.2', messages=[{'role': 'user', 'content':PROMPT.format(language=language)
+}])
+    return response['message']['content'] 
+
+if __name__ == '__main__':
+    language = input("Enter the programming language: ")
+    dockerfile = generate_dockerfile(language)
+    print("\nGenerated Dockerfile:\n")
+    print(dockerfile)
---
+++ generate_dockerfile_hosted.py
@@ -0,0 +1,29 @@
+import google.generativeai as genai
+import os
+
+os.environ['GOOGLE_API_KEY'] = "AIzaSyCOz7EgUbFIp0WoUpaWQnWjD9tFMGGeGp8"
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+PROMPT = """
+ONLY Generate an ideal dockerfile for {language} with best practices. Just sharet the dockerfile content without any explanation 
+between two lines to make it easy to copy.
+Include:
+- Base image
+- Installing dependencies
+- Setting working directory
+- Adding source code
+- Running the application
+- Multi stage docker build
+ """
+
+def generate_dockerfile(language):
+    response = model.generate_content(PROMPT.format(language=language))
+    return response.text
+
+if __name__ == '__main__':
+    language = input("Enter the programming language: ")
+    dockerfile = generate_dockerfile(language)
+    print("\nGenerated Dockerfile:\n")
+    print(dockerfile)
